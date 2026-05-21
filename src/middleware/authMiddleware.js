85c3d03dfbc6eba7a9cd7737880eb4dc07f36771
Alfonso Conejo <49042515+AlfonsoConejo@users.noreply.github.com>
2026-05-21 00:09:54 -0600

    Finished authMiddleware
    -Middleware is executed before entering "auth/me" endpoint.

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,28 +1,24 @@
 import jwt from "jsonwebtoken"
 
-export const authMiddlaware = (req, res, next) => {
-  const authHeader = req.cookies.token;
-  if(!authHeader) {
+export const authMiddleware = (req, res, next) => {
+  const token = req.cookies.accessToken;
+  if(!token) {
     return res.status(401).json({
       message: "Acceso denegado"
     });
   }
-
-  const token = authHeader.split(" ")[1];
   
-  jwt.verify(
-    token,
-    process.env.JWT_SECRET,
-    (error, decoded) => {
-      if (error) {
-        return res.status(403).json({
-          message: "Token inválido"
-        });
-      }
+  try{
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_ACCESS_SECRET);
 
       req.user = decoded;
 
       next();
-    }
-  );
+  } catch (error) {
+    return res.status(403).json({
+      message: "Token inválido"
+    });
+  }
 };
--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { register, login, me, refresh, logout } from "../auth/auth.controller.js";
+import { authMiddleware } from "../../middleware/authMiddleware.js";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/me", me);
+router.get("/me", authMiddleware, me);
 router.post("/refresh", refresh);
 router.post("/logout", logout);
 
