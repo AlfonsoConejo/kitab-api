1bbb81548419fdb9cec6c38d27169811fa715401
Alfonso Conejo <49042515+AlfonsoConejo@users.noreply.github.com>
2026-06-11 20:33:29 -0600

    Middleware detects Expired and Invalid tokens

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,22 +2,34 @@ import jwt from "jsonwebtoken"
 
 export const authMiddleware = (req, res, next) => {
   const token = req.cookies.accessToken;
-  if(!token) {
+
+  if (!token) {
     return res.status(401).json({
+      code: "NO_ACCESS_TOKEN",
       message: "Acceso denegado"
     });
   }
-  
-  try{
+
+  try {
     const decoded = jwt.verify(
       token,
-      process.env.JWT_ACCESS_SECRET);
+      process.env.JWT_ACCESS_SECRET
+    );
 
-      req.user = decoded;
+    req.user = decoded;
 
-      next();
+    next();
   } catch (error) {
+
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({
+        code: "ACCESS_TOKEN_EXPIRED",
+        message: "Access token expired"
+      });
+    }
+
     return res.status(403).json({
+      code: "INVALID_ACCESS_TOKEN",
       message: "Token inválido"
     });
   }
