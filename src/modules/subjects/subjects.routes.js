27a31bfa456bb0aeb1bae869eb04f06726fb97c8
Alfonso Conejo <49042515+AlfonsoConejo@users.noreply.github.com>
2026-06-17 18:50:17 -0600

    Removed "getSubjects" import

--- src/modules/subjects/subjects.routes.js
+++ src/modules/subjects/subjects.routes.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { getSubjects } from "../subjects/subjects.controller.js";
 import { authMiddleware } from "../../middleware/authMiddleware.js";
 
 const router = Router();
